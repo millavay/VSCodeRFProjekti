b34b3c7d6c95eb33cac88c4523534eacc90f22f8
meanarhi <amk1005828@student.hamk.fi>
2026-03-07 22:39:11 +0200

    kommentteja

--- ibanvalidate.py
+++ ibanvalidate.py
@@ -1,13 +1,16 @@
 def IsIbanCorrect(iban):
+    # Remove spaces and convert to uppercase
     cleaned = iban.replace(' ', '').upper()
     
+    #tarkistetaan onko pituus oikea, suomen IBAN on 18 merkkiä pitkä
     if len(cleaned) != 18:
         return False
     
     #return True
     
-    
+    #siirretään ekat neljä merkkiä loppuun, osa IBAN tarkistusta
     rearranged = cleaned[4:] + cleaned[:4]
+    #korvataan kirjaimet numeroilla
     numeric = ''.join([str(ord(c) - 55) if c.isalpha() else c for c in rearranged])
-    
+    #tarkistetaan jakojäännös 97:llä, IBAN tarkistus vaatii, että jakojäännös on 1
     return int(numeric) % 97 == 1
